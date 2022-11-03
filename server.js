b4cc165f12676bf173c4000b0f2049340a564efd
Angel G Guerra-Marquez <agguerra-marquez@csuchico.edu>
2022-11-03 13:15:24 -0700

    update of ex

--- server.js
+++ server.js
@@ -7,11 +7,18 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
+const app = express();
+function unixTimestamp () {  
+    return Math.floor(
+      (Date.now()/1000)|0
+    )
+  }
+app.get('/', (req, res,) => {
+    var name = " my name is Angel Guerra Marquez"; 
+    res.send({message: name, 
+    "timestamp": (unixTimestamp()).toString()});
+});
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
